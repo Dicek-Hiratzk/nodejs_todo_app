4d04deaaa4a1a68da0760289ff033168e301abf4
Dicek-Hiratzk <diezoc24@gmail.com>
2024-12-31 21:49:38 +0900

    app.jsのconnectDB周りを編集

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use("/api/v1/tasks", taskRoute);
 // データベースと接続 Cluster0 非同期処理
 const start = async () => {
   try {
-    await connectDB(process.env.MONGO_URL);
+    await connectDB(process.env.MONGO_DEPLOY_URL || process.env.MONGO_URL); // 本番環境用の環境変数KEY || 開発環境用
     app.listen(process.env.PORT || PORT, console.log("サーバーが起動しました"));
   } catch (err) {
     console.log(err);
